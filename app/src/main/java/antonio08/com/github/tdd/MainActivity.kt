b08501ac944679bc08a252bf5b073989476fe96b
Antonio <antonio_lozano@outlook.com>
2020-02-04 19:46:04 -0600

    Pushing project

---
+++ app/src/main/java/antonio08/com/github/tdd/MainActivity.kt
@@ -0,0 +1,12 @@
+package antonio08.com.github.tdd
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
