a5884f6ecad9ef6f0556825505a2db6e0feac87c
Antonio <antonio_lozano@outlook.com>
2020-02-06 21:29:21 -0600

    TDD-004 Build dummy project + Adding Student Attendance Application

---
+++ app/src/main/java/antonio08/com/github/tdd/StudentAttendenceApplication.kt
@@ -0,0 +1,13 @@
+/**
+ * Created by Antonio Lozano
+ *
+ * 02/06/2020
+ *
+ */
+package antonio08.com.github.tdd
+
+import android.app.Application
+
+
+class StudentAttendenceApplication : Application() {
+}
